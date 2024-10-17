b1947cc67ef60981e12b4c7b0e4e744b49e5136e
jackberck <berckjack@gmail.com>
2024-10-17 23:27:56 +0700

    feat: adding hero swiper

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       colors: {
         "blue-base": "#1a73e8",
         "dark-base": "#111827",
